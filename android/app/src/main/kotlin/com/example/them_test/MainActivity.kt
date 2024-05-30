59d9ee01bbb514df085456cc775853dd16939e53
AhmedHr22 <ahmed.work.2200@gmail.com>
2024-05-30 11:58:54 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/them_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.them_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/theme/dark_theme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+  final DarkTheme = ThemeData(
+      appBarTheme: AppBarTheme( backgroundColor: Colors.deepPurple.shade900,foregroundColor: Colors.white, ),
+      backgroundColor: Colors.deepPurple.shade900,
+
+  );
+
+  final LightTheme = ThemeData(
+    appBarTheme: AppBarTheme(backgroundColor: Colors.deepPurple.shade300,foregroundColor: Colors.black),
+    backgroundColor: Colors.deepPurple.shade100,
+  );
+
+
